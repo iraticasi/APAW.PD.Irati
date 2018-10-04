bfb3199f60c08926f70eaa9a421f187d32563964
iraticasi <iraticasi@gmail.com>
2018-10-04 13:48:03 +0200

    deleted some constructors in Song

--- src/test/java/music/models/SongBuilderTest.java
+++ src/test/java/music/models/SongBuilderTest.java
@@ -13,6 +13,7 @@ class SongBuilderTest {
         LocalDateTime date = LocalDateTime.now();
         Song song = new SongBuilder("1", "my song").singer("my singer")
                 .seconds(200).date(date).genre(Genre.CLASSIC).build();
+        assertEquals("1", song.getId());
         assertEquals("my song", song.getName());
         assertEquals("my singer", song.getSinger());
         assertEquals(200, song.getSeconds());
