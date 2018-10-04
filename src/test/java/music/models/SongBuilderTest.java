393940c4a1cd26af5d448c7e7c0eeeb17a3b43cf
iraticasi <iraticasi@gmail.com>
2018-10-04 13:46:44 +0200

    test SongBuilder created and passed

---
+++ src/test/java/music/models/SongBuilderTest.java
@@ -0,0 +1,22 @@
+package music.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongBuilderTest {
+
+    @Test
+    void TestFull(){
+        LocalDateTime date = LocalDateTime.now();
+        Song song = new SongBuilder("1", "my song").singer("my singer")
+                .seconds(200).date(date).genre(Genre.CLASSIC).build();
+        assertEquals("my song", song.getName());
+        assertEquals("my singer", song.getSinger());
+        assertEquals(200, song.getSeconds());
+        assertEquals(date, song.getDate());
+        assertEquals(Genre.CLASSIC, song.getGenre());
+    }
+}
