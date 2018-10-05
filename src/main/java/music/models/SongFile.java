e57a5c7a3527be8291bb8b2de64b850ddfa39387
iraticasi <iraticasi@gmail.com>
2018-10-05 13:47:54 +0200

    class SongFile implemented

--- src/main/java/music/models/SongFile.java
+++ src/main/java/music/models/SongFile.java
@@ -1,4 +1,43 @@
 package music.models;
 
+import java.util.List;
+
 public class SongFile extends File{
+
+    Song song;
+
+    public SongFile(Song song) {
+        super(song.getName());
+        this.song = song;
+    }
+
+    @Override
+    public void add(File file) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    @Override
+    public void remove(File file) {
+        throw new UnsupportedOperationException("Operación no soportada");
+
+    }
+
+    @Override
+    public boolean isFolder() {
+        return false;
+    }
+
+    @Override
+    public String view() {
+        return this.song.getId();
+    }
+
+    @Override
+    public List<File> listFiles() {
+        return null;
+    }
+
+    public Song getSong(){
+        return this.song;
+    }
 }
